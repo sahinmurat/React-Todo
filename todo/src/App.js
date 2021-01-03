b4e1ab221ebe143ca9acad87073386988d6def42
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-03 10:36:55 +0100

    app started

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form'
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1> Murat's TODO List </h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
